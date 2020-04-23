5dcd1c56300c8864b1d823f51841a884ba660b78
Joseph Chou <joe.nicu@gmail.com>
2020-04-23 19:00:36 -0400

    Initial commit

---
+++ flowers.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+
+data <- read.csv(file = 'flowers.csv', stringsAsFactors = FALSE) %>% # type + color will be factors
+  as_tibble() %>%
+  rename_all(tolower) %>%
+  select(-id) %>%
+  mutate(
+    seedbag = (seedbag != 0),
+    breedtrue = ((red != 1) & (yellow != 1) & (white != 1) & (brightness != 1))
+  )
+
+data %>%
+  select(type, color) %>%
+  unique %>%
+  group_by(type) %>%
+  summarise(num_color = n()) %>%
+  arrange(desc(num_color))
+
+
+# data %>%
+#   filter(red != 1, yellow != 1, white != 1, brightness != 1) %>%
+#   select(type, color) %>%
+#   unique %>%
+#   group_by(type) %>%
+#   summarise(num_color = n()) %>%
+#   arrange(desc(num_color))
+# 
+
+cross_genotypes <- function(g1, g2) {
+  # g1 / g2 genotypes: either 0, 1, or 2
+  # return possible genotypes
+  g <- sort(c(g1, g2))
+  if (all(g == c(0,0))) {
+    result <- c(0)
+  } else if (all(g == c(0,1))) {
+    result <- c(0, 1)
+  } else if (all(g == c(0,2))) {
+    result <- c(1)
+  } else if (all(g == c(1,1))) {
+    result <- c(0, 1, 1, 2)
+  } else if (all(g == c(1,2))) {
+    result <- c(1, 2)
+  } else if (all(g == c(2,2))) {
+    result <- c(2)
+  }
+  return(result)
+}
+
+cross_flower <- function(type, r1, y1, w1, r2, y2, w2, b1 = 0, b2 = 0) {
+  expand.grid(
+    type = type,
+    red = cross_genotypes(r1, r2),
+    yellow = cross_genotypes(y1, y2),
+    white = cross_genotypes(w1, w2),
+    brightness = cross_genotypes(b1, b2),
+    stringsAsFactors = FALSE
+  ) %>%
+    as.tibble() %>%
+    left_join(data %>% select(-seedbag), by = c("type", "red", "yellow", "white", "brightness")) %>%
+    group_by(type,red,yellow,white,brightness, color, breedtrue) %>%
+    summarise(n=n()) %>%
+    ungroup() %>%
+    group_by(color) %>%
+    mutate(prop = n / sum(n)) %>%
+    select(-n) %>%
+    arrange(color, desc(prop))
+}
+
+cross_flower('Hyacinth',2,1,0,2,1,0)
+
